51c077ba9a7a201abed7835c736525fe24c0dc60
Billy Chiu <billychiu517@gmail.com>
2023-10-09 16:51:27 -0700

    deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,15 +4,33 @@ const config = require('./index');
 module.exports = {
   development: {
     storage: config.dbFile,
-    dialect: "sqlite",
+    dialect: "postgres",
     seederStorage: "sequelize",
     logQueryParameters: true,
-    typeValidation: true
+    typeValidation: true,
   },
+  // development: {
+  //   storage: config.dbFile,
+  //   dialect: "postgres",
+  //   seederStorage: "sequelize",
+  //   logQueryParameters: true,
+  //   typeValidation: true,
+  //   username: "postgres",
+  //   password: "billyjr123",
+  //   database: 'postgres',
+  //   host: "localhost",
+  //   port: 5432,
+  // },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    // use_env_variable: 'DATABASE_URL',
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    port: process.env.PORT,
     dialectOptions: {
       ssl: {
         require: true,
