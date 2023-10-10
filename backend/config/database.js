6e83907ed915748d82bc0a15395f0e7916d19f12
Billy Chiu <billychiu517@gmail.com>
2023-10-09 23:11:02 -0700

    theme

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,46 +2,47 @@
 const config = require('./index');
 
 module.exports = {
-  development: {
-    // use_env_variable: 'DATABASE_URL',
-    storage: config.dbFile,
-    // use_env_variable: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-    username: "default",
-    password: "XBy6wI9ibYcH",
-    database: "verceldb",
-    host: "ep-bitter-feather-72486321-pooler.us-west-2.postgres.vercel-storage.com",
-    port: 5432,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    define: {
-      schema: "public"
-    }
-  },
   // development: {
+  //   // use_env_variable: 'DATABASE_URL',
   //   storage: config.dbFile,
-  //   dialect: "postgres",
-  //   seederStorage: "sequelize",
-  //   logQueryParameters: true,
-  //   typeValidation: true,
-  // },
-  // development: {
-  //   storage: config.dbFile,
-  //   dialect: "postgres",
-  //   seederStorage: "sequelize",
-  //   logQueryParameters: true,
-  //   typeValidation: true,
-  //   username: "postgres",
-  //   password: "billyjr123",
-  //   database: 'postgres',
+  //   // use_env_variable: process.env.DATABASE_URL,
+  //   dialect: 'postgres',
+  //   seederStorage: 'sequelize',
+  //   username: "default",
+  //   password: "XBy6wI9ibYcH",
+  //   database: "verceldb",
   //   host: "localhost",
-  //   port: 5432,
+  //   // host: "ep-bitter-feather-72486321-pooler.us-west-2.postgres.vercel-storage.com",
+  //   port: 3000,
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false
+  //     }
+  //   },
+  //   define: {
+  //     schema: "public"
+  //   }
   // },
+  // development: {
+  //   storage: config.dbFile,
+  //   dialect: "postgres",
+  //   seederStorage: "sequelize",
+  //   logQueryParameters: true,
+  //   typeValidation: true,
+  // },
+  development: {
+    storage: config.dbFile,
+    dialect: "postgres",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true,
+    username: "postgres",
+    password: "billyjr123",
+    database: 'postgres',
+    host: "localhost",
+    port: 5432,
+  },
   // production: {
   //   use_env_variable: 'DATABASE_URL',
   //   // use_env_variable: process.env.DATABASE_URL,
