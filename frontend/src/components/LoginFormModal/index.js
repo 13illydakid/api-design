da4ea000dda7f5d387f09aae070b8d52b93b9867
Billy Chiu <billychiu517@gmail.com>
2024-01-26 18:41:59 -0800

    login modal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -17,11 +17,11 @@ function LoginFormModal() {
 
   useEffect(() => {
 
-    if(password.length < 6 || credential.length < 4){
+    if (password.length < 6 || credential.length < 4) {
 
       setEmptyField(true);
 
-    }else setEmptyField(false);
+    } else setEmptyField(false);
 
   }, [credential, password])
 
@@ -45,7 +45,18 @@ function LoginFormModal() {
       .then(closeModal)
   }
 
+  const exitLogin = (e) => {
+    e.preventDefault();
+    closeModal();
+  }
+
   return (
+    <>
+      <span className="flex justify-end">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:bg-black rounded-lg hover:text-white cursor-pointer" onClick={exitLogin}>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+      </span>
       <div className="login-form-container">
         <i className="login-logo-left fa-solid fa-brands fa-airbnb"></i>
         <i className="fa-solid login-logo-text"><h2>LOGIN</h2></i>
@@ -58,8 +69,8 @@ function LoginFormModal() {
           <div className="login-input">
             <label>
               Username or Email
-             </label>
-             <div>
+            </label>
+            <div>
               <input
                 className="input-box"
                 type="text"
@@ -68,7 +79,7 @@ function LoginFormModal() {
                 required
               />
               <div className="login-errors">{errors.username}</div>
-             </div>
+            </div>
           </div>
           <div className="login-input">
             <label>
@@ -88,17 +99,19 @@ function LoginFormModal() {
           <div className="login-button-container">
             <button
               type="submit"
-              className={emptyField ? "login-disabled" : "login-submit-button"}
+              // className={emptyField ? "login-disabled" : "login-submit-button"}
+              className={emptyField ? "hover:cursor-not-allowed" : "hover:text-black"}
               disabled={Boolean(emptyField)}
             >
-              LOGIN
+              Continue
             </button>
-          <div className="demo-button-container">
-            <Link to='/' onClick={demoUserLogin} className='demo-user-login'>Log in as Demo User</Link>
           </div>
-          </div>
+            <div className="demo-button-container">
+              <Link to='/' onClick={demoUserLogin} className='demo-user-login'>Log in as Demo User</Link>
+            </div>
         </form>
       </div>
+    </>
 
   );
 }
